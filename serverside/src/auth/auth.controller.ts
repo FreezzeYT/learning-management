7c0032e6f9d3b4ac1d6bfc9b67fee309c65defb3
FreezzeYT <rbalajipctechchannel@gmail.com>
2025-03-25 10:33:11 +0530

    Added role guard

--- serverside/src/auth/auth.controller.ts
+++ serverside/src/auth/auth.controller.ts
@@ -1,15 +1,28 @@
-import { Body, Controller, Post, UseGuards, Get, Request, UsePipes, ValidationPipe, BadRequestException } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { CreateUserData } from "dtos/create-user.dto";
-import { LoginUsrDto } from "dtos/user-login.dto";
-import { JwtAuthGuard } from "./jwt-auth-guard";
-import { UserService } from "src/user/user.service";
-
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Get,
+  Request,
+  UsePipes,
+  ValidationPipe,
+  BadRequestException,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserData } from 'dtos/create-user.dto';
+import { LoginUsrDto } from 'dtos/user-login.dto';
+import { JwtAuthGuard } from './jwt-auth-guard';
+import { UserService } from 'src/user/user.service';
+import { RolesGuard } from './roles.guard';
+import { Roles } from 'src/decorators/roles.decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService, private userservice: UserService) { }
-
+  constructor(
+    private authService: AuthService,
+    private userservice: UserService,
+  ) {}
 
   @Post('register')
   @UsePipes(new ValidationPipe({ whitelist: true }))
@@ -18,27 +31,24 @@ export class AuthController {
       throw new BadRequestException('All fields are required');
     }
     return this.authService.register(UserDto);
-  }  
-
-
+  }
 
   @Post('login')
-  async login(@Body() loginDTO: LoginUsrDto){
-    const user = await this.userservice.findbyusername(loginDTO.username)
-    
+  async login(@Body() loginDTO: LoginUsrDto) {
+    const user = await this.userservice.findbyusername(loginDTO.username);
+
     return this.authService.login(loginDTO);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('Admin', 'Instructor', 'Student')
   @Get('profile')
-  getProfile(@Request() req){
-    
-    
-     return { 
-      username: req.user.username, 
-      name: req.user.name || "Name missing", 
-      _id: req.user._id ,
-      role:req.user.role || "Role missing"
-  };
+  getProfile(@Request() req) {
+    return {
+      username: req.user.username,
+      name: req.user.name || 'Name missing',
+      _id: req.user._id,
+      role: req.user.role || 'Role missing',
+    };
   }
 }
